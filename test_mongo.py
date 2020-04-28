42b4c2c20a93f577a2aa08f75c9b610341789a13
Itumeleng091slk <59916899+Itumeleng091slk@users.noreply.github.com>
2020-04-28 17:19:07 +0200

    Create test_mongo.py

---
+++ test_mongo.py
@@ -0,0 +1,25 @@
+import unittest
+from mongo_engine_code import *
+
+class Test_mongo(unittest.TestCase):
+
+    def test_Create_Visitor(self):
+        assert Create_Visitor
+    
+    def test_list_Vistors(self):
+        assert Create_Visitor
+
+    def test_delete_Visitor(self):
+        assert Create_Visitor
+
+    def test_delete_all(self):
+        assert Create_Visitor
+
+    def test_update_Visitor(self):
+        assert Create_Visitor
+
+    def test_list_all_details(self):
+        assert Create_Visitor
+
+if __name__ == '__main__':
+    unittest.main()
