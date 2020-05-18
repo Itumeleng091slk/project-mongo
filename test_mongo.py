e9678de1131ce0277c676bba9370122a67301a64
Itumeleng091slk <59916899+Itumeleng091slk@users.noreply.github.com>
2020-05-18 21:43:08 +0200

    Update test_mongo.py

--- test_mongo.py
+++ test_mongo.py
@@ -1,25 +1,28 @@
 import unittest
-from mongo_engine_code import *
+from mongo_engine_code import Visitor
+
+visitors = Visitor()
 
 class Test_mongo(unittest.TestCase):
 
     def test_Create_Visitor(self):
-        assert Create_Visitor
+        assert visitors.Create_Visitor
     
     def test_list_Vistors(self):
-        assert Create_Visitor
+        assert visitors.list_Vistors
 
     def test_delete_Visitor(self):
-        assert Create_Visitor
+        assert visitors.delete_Visitor
 
     def test_delete_all(self):
-        assert Create_Visitor
+        assert visitors.delete_all
 
     def test_update_Visitor(self):
-        assert Create_Visitor
+        assert visitors.update_Visitor
 
     def test_list_all_details(self):
-        assert Create_Visitor
+        assert visitors.list_all_details
+
 
 if __name__ == '__main__':
     unittest.main()
