e8658cc6b8dbb6b87930b4bff8ea82bf79c9efbd
Itumeleng091slk <59916899+Itumeleng091slk@users.noreply.github.com>
2020-05-26 18:28:21 +0200

    update commit

---
+++ visitors_data.py
@@ -0,0 +1,5 @@
+mylist = [
+  { "visitor_name": "Scott", "visitors_age": 24, "date of visit": "2020-19-4", "time of visit": "9:00 am", "assistance_name": "Kevin", "comments": "He was very helpful and polite"},
+  { "visitor_name": "Carry", "visitors_age": 28, "date of visit": "2020-21-8", "time of visit": "11:00 am", "assistance_name": "Lou", "comments": "He was very impatient"},
+  { "visitor_name": "Viola", "visitors_age": 21, "date of visit": "2020-5-5", "time of visit": "13:00 pm", "assistance_name": "Sam", "comments": "She was kind"}, 
+]
