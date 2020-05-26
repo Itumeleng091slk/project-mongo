7a9f09ddd9f27f97622a7e7da6837df6887b10d5
Itumeleng091slk <59916899+Itumeleng091slk@users.noreply.github.com>
2020-05-26 18:27:22 +0200

    update commit

--- test_mongo.py
+++ test_mongo.py
@@ -1,28 +1,33 @@
 import unittest
+from visitors_data import mylist
 from mongo_engine_code import Visitor
 
-visitors = Visitor() #Creating an instance
+class Test_mongo(unittest.TestCase):   
 
-class Test_mongo(unittest.TestCase):
-
-    def test_Create_Visitor(self):
-        assert visitors.Create_Visitor
+    def test_create_visitor(self):
+        visitors = Visitor(mylist)
+        assert visitors.create_visitor() == "visitor created"
     
-    def test_list_Vistors(self):
-        assert visitors.list_Vistors
+    def test_visitor_not_created(self):
+        visitors = Visitor("this is a string")
+        assert visitors.create_visitor() == 'visitor not created'
 
     def test_delete_Visitor(self):
-        assert visitors.delete_Visitor
+        visitors = Visitor(mylist)
+        assert visitors.delete_visitor("Viola") == "visitor delete
 
-    def test_delete_all(self):
-        assert visitors.delete_all
+    def visitor_updated(self):
+        visitors = Visitor("this is a string")
+        assert visitors.update_visitor("Scott", "male") == "visitor updated"
 
-    def test_update_Visitor(self):
-        assert visitors.update_Visitor
+    def test_visitor_not_updated(self):
+        visitors = Visitor(mylist)
+        assert visitors.update_visitor("Scott", "male") != "visitor not updated"
 
     def test_list_all_details(self):
-        assert visitors.list_all_details
-
+        visitors = Visitor(mylist)
+        assert visitors.list_all_details()
 
 if __name__ == '__main__':
     unittest.main()
+
